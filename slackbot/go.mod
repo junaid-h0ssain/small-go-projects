050d933226f3a60476290796a2cf4378a3008738
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-02-17 02:12:21 +0600

    basic template has created for slack bot

--- slackbot/go.mod
+++ slackbot/go.mod
@@ -1,3 +1,14 @@
 module slackbot
 
 go 1.25.0
+
+require github.com/shomali11/slacker v1.4.1
+
+require (
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/robfig/cron v1.2.0 // indirect
+	github.com/shomali11/commander v0.0.0-20220716022157-b5248c76541a // indirect
+	github.com/shomali11/proper v0.0.0-20180607004733-233a9a872c30 // indirect
+	github.com/slack-go/slack v0.13.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+)
--- slackbot/main.go
+++ slackbot/main.go
@@ -1,1 +1,36 @@
 package slackbot
+
+import (
+	//"github.com/slack-go/slack"
+	"github.com/shomali11/slacker"
+	"fmt"
+	"log"
+	"os"
+	"context"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent){
+	for event := range analyticsChannel{
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+	}
+}
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", os.Getenv("SLACK_BOT_TOKEN"))
+	os.Setenv("SLACK_APP_TOKEN", os.Getenv("SLACK_APP_TOKEN"))
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	go printCommandEvents(bot.CommandEvents())
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
